1c5d3e8a53f04e68fecde3c3bfc16865b177d8c5
Nadezhda Kosyura <nadejdakna2002@gmail.com>
2023-04-06 12:45:21 +0300

    Добавлены тесты для Sapper

--- src/test/SapperTest.java
+++ src/test/SapperTest.java
@@ -2,39 +2,103 @@ package test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sapper.Cell;
+import sapper.Game;
 import sapper.Sapper;
+import sapper.State;
+import test.environment_generator.TestEnvironment_generator;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SapperTest {
-    //TODO
-
     private Sapper sapper;
+    private Game game;
 
     @BeforeEach
     public void testSetup() {
+        game = new Game(new TestEnvironment_generator(), new Sapper(3, 3));
+        sapper = game.getSapper();
     }
 
     @Test
     public void test_demine() {
+        Cell minedCell = game.getGameField().getCell(new Point(2,2));
+        sapper.demine(minedCell);
 
+        assertEquals(2, sapper.getFlag());
+        assertEquals(State.FLAG, minedCell.getState());
     }
 
     @Test
     public void test_openEmptyCell() {
+        Cell emptyCell = game.getGameField().getCell(new Point(0,0));
+        sapper.openCell(emptyCell);
 
+        assertEquals(State.OPEN, emptyCell.getState());
+        assertEquals(State.OPEN, game.getGameField().getCell(new Point(1,0)));
+        assertEquals(1, game.getGameField().getCell(new Point(1,0)).getNumOfNeighboringMines());
+        assertEquals(State.OPEN, game.getGameField().getCell(new Point(0,1)));
+        assertEquals(1, game.getGameField().getCell(new Point(0,1)).getNumOfNeighboringMines());
+        assertEquals(State.OPEN, game.getGameField().getCell(new Point(1,1)));
+        assertEquals(3, game.getGameField().getCell(new Point(1,1)).getNumOfNeighboringMines());
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,2)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,2)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,2)));
+    }
+
+    @Test
+    public void test_openCellWithNumber() {
+        Cell numberCell = game.getGameField().getCell(new Point(2,1));
+        sapper.openCell(numberCell);
+
+        assertEquals(State.OPEN, numberCell.getState());
+        assertEquals(2, numberCell.getNumOfNeighboringMines());
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,2)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,2)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,2)));
     }
 
     @Test
     public void test_openMinedCell() {
+        Cell minedCell = game.getGameField().getCell(new Point(2,2));
+        sapper.openCell(minedCell);
 
+        assertEquals(State.OPEN, minedCell.getState());
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,0)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(2,1)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(1,2)));
+        assertEquals(State.CLOSE, game.getGameField().getCell(new Point(0,2)));
+        assertEquals(2, game.getSapper().getLife());
     }
 
     @Test
     public void test_removeFlagFromCellWithFlag() {
+        Cell flagCell = game.getGameField().getCell(new Point(2,2));
+        flagCell.setFlag();
+        assertEquals(2, game.getSapper().getFlag());
 
+        sapper.removeFlag(flagCell);
+        assertEquals(3, game.getSapper().getFlag());
     }
 
     @Test
     public void test_removeFlagFromCellWithoutFlag() {
+        Cell flagCell = game.getGameField().getCell(new Point(2,2));
 
+        sapper.removeFlag(flagCell);
+        assertEquals(3, game.getSapper().getFlag());
     }
 }
